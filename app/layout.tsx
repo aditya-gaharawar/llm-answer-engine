a70b8e0f9f3c9c87e55fad80e1896a0ac0d0f019
aditya-gaharawar <164782232+aditya-gaharawar@users.noreply.github.com>
2024-04-13 01:41:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,25 +12,18 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'WEBSPACEAI'
 const description =
-  'A fully open-source AI-powered answer engine with a generative UI.'
+  'Search Engine Build on Genrative AI .'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://morphic.sh'),
+  metadataBase: new URL('https://webspaceai.in'),
   title,
   description,
   openGraph: {
     title,
     description
   },
-  twitter: {
-    title,
-    description,
-    card: 'summary_large_image',
-    creator: '@miiura'
-  }
-}
 
 export const viewport: Viewport = {
   width: 'device-width',
