22ac1196c195f1550ae1754b8eedee403164c9ed
aditya-gaharawar <164782232+aditya-gaharawar@users.noreply.github.com>
2024-04-20 11:23:38 +0530

    Update researcher.tsx

--- lib/agents/researcher.tsx
+++ lib/agents/researcher.tsx
@@ -12,164 +12,58 @@ import { ToolBadge } from '@/components/tool-badge'
 import { SearchSkeleton } from '@/components/search-skeleton'
 import { SearchResults } from '@/components/search-results'
 import { BotMessage } from '@/components/message'
-import { SearchResultsImageSection } from '@/components/search-results-image'
 import Exa from 'exa-js'
+import { SearchResultsImageSection } from '@/components/search-results-image'
 
 export async function researcher(
-  uiStream: ReturnType<typeof createStreamableUI>,
-  streamText: ReturnType<typeof createStreamableValue<string>>,
-  messages: ExperimentalMessage[]
-) {
-  const openai = new OpenAI({
-    baseUrl: process.env.OPENAI_API_BASE, // optional base URL for proxies etc.
-    apiKey: process.env.OPENAI_API_KEY, // optional API key, default to env property OPENAI_API_KEY
+	@@ -26,7 +25,7 @@ export async function researcher(
     organization: '' // optional organization
   })
 
-  const searchAPI: 'bing' | 'exa' = 'bing'
+  const searchAPI: 'tavily' | 'exa' = 'tavily'
 
   let fullResponse = ''
   const answerSection = (
-    <Section title="Answer">
-      <BotMessage content={streamText.value} />
-    </Section>
-  )
-
-  const result = await experimental_streamText({
-    model: openai.chat('gpt-4-turbo'),
-    maxTokens: 2500,
-    system: `As a professional search expert, you possess the ability to search for any information on the web. 
-    For each user query, utilize the search results to their fullest potential to provide additional information and assistance in your response.
-    If there are any images relevant to your answer, be sure to include them as well.
-    Aim to directly address the user's question, augmenting your response with insights gleaned from the search results.
-    Whenever quoting or referencing information from a specific URL, always cite the source URL explicitly.
-    `,
-    messages,
-    tools: {
-      search: {
-        description: 'Search the web for information',
-        parameters: searchSchema,
-        execute: async ({
-          query,
-          max_results,
-          search_depth
-        }: {
-          query: string
-          max_results: number
-          search_depth: 'basic' | 'advanced'
-        }) => {
-          uiStream.update(
-            <Section>
-              <ToolBadge tool="search">{`${query}`}</ToolBadge>
-            </Section>
-          )
-
-          uiStream.append(
-            <Section>
-              <SearchSkeleton />
-            </Section>
+	@@ -71,8 +70,8 @@ export async function researcher(
           )
 
           const searchResult =
-            searchAPI === 'bing'
-              ? await bingSearch(query, max_results, search_depth)
+            searchAPI === 'tavily'
+              ? await tavilySearch(query, max_results, search_depth)
               : await exaSearch(query)
 
           uiStream.update(
-            <Section title="Images">
-              <SearchResultsImageSection
-                images={searchResult.images}
-                query={searchResult.query}
-              />
-            </Section>
-          )
-          uiStream.append(
-            <Section title="Sources">
-              <SearchResults results={searchResult.results} />
-            </Section>
-          )
-
-          uiStream.append(answerSection)
-
-          return searchResult
-        }
-      }
-    }
-  })
-
-  const toolCalls: ToolCallPart[] = []
-  const toolResponses: ToolResultPart[] = []
-  for await (const delta of result.fullStream) {
-    switch (delta.type) {
-      case 'text-delta':
-        if (delta.textDelta) {
-          // If the first text delata is available, add a ui section
-          if (fullResponse.length === 0 && delta.textDelta.length > 0) {
-            // Update the UI
-            uiStream.update(answerSection)
-          }
-
-          fullResponse += delta.textDelta
-          streamText.update(fullResponse)
-        }
-        break
-      case 'tool-call':
-        toolCalls.push(delta)
-        break
-      case 'tool-result':
-        toolResponses.push(delta)
-        break
-      case 'error':
-        fullResponse += `\nError occurred while executing the tool`
-        break
-    }
-  }
-  messages.push({
-    role: 'assistant',
-    content: [{ type: 'text', text: fullResponse }, ...toolCalls]
-  })
-
-  if (toolResponses.length > 0) {
-    // Add tool responses to the messages
-    messages.push({ role: 'tool', content: toolResponses })
-  }
-
+	@@ -137,25 +136,24 @@ export async function researcher(
   return { result, fullResponse }
 }
 
-async function bingSearch(
+async function tavilySearch(
   query: string,
   maxResults: number = 10,
   searchDepth: 'basic' | 'advanced' = 'basic'
 ): Promise<any> {
-  const apiKey = process.env.BING_API_KEY
-
-  if (!apiKey) {
-    throw new Error('BING_API_KEY environment variable is not set')
-  }
-
-  const endpoint = 'https://api.bing.microsoft.com/v7.0/search'
-
-  const response = await fetch(endpoint, {
-    method: 'GET',
+  const apiKey = process.env.TAVILY_API_KEY
+  const response = await fetch('https://api.tavily.com/search', {
+    method: 'POST',
     headers: {
-      'Ocp-Apim-Subscription-Key': apiKey
+      'Content-Type': 'application/json'
     },
-    params: {
-      q: query,
-      count: maxResults,
-      safeSearch: 'Off'
-    }
+    body: JSON.stringify({
+      api_key: apiKey,
+      query,
+      max_results: maxResults < 5 ? 5 : maxResults,
+      search_depth: searchDepth,
+      include_images: true,
+      include_answers: true
+    })
   })
 
   if (!response.ok) {
     throw new Error(`Error: ${response.status}`)
   }
-
   const data = await response.json()
   return data
 }
-
 async function exaSearch(query: string, maxResults: number = 10): Promise<any> {
   const apiKey = process.env.EXA_API_KEY
   const exa = new Exa(apiKey)
