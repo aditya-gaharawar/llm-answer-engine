11908b677b2383d49fdbf7e6d689635b4afa872b
aditya-gaharawar <164782232+aditya-gaharawar@users.noreply.github.com>
2024-04-25 00:57:54 +0530

    Update researcher.tsx

--- lib/agents/researcher.tsx
+++ lib/agents/researcher.tsx
@@ -7,34 +7,143 @@ import {
 } from 'ai'
 import { searchSchema } from '@/lib/schema/search'
 import { Section } from '@/components/section'
-import { OpenAI } from 'ai/openai'
-import { ToolBadge } from '@/components/tool-badge'
-import { SearchSkeleton } from '@/components/search-skeleton'
-import { SearchResults } from '@/components/search-results'
+import { OpenAI } from '@ai-sdk/openai'
 import { BotMessage } from '@/components/message'
 import Exa from 'exa-js'
-import { SearchResultsImageSection } from '@/components/search-results-image'
+import { Card } from '@/components/ui/card'
+import { SearchResults } from '../types'
+import { SearchSection } from '@/components/search-section'
 
 export async function researcher(
-	@@ -26,7 +25,7 @@ export async function researcher(
+  uiStream: ReturnType<typeof createStreamableUI>,
+  streamText: ReturnType<typeof createStreamableValue<string>>,
+  messages: ExperimentalMessage[],
+  useSpecificModel?: boolean
+) {
+  const openai = new OpenAI({
+    baseUrl: process.env.OPENAI_API_BASE, // optional base URL for proxies etc.
+    apiKey: process.env.OPENAI_API_KEY, // optional API key, default to env property OPENAI_API_KEY
     organization: '' // optional organization
   })
 
   const searchAPI: 'tavily' | 'exa' = 'tavily'
 
   let fullResponse = ''
+  let hasError = false
   const answerSection = (
-	@@ -71,8 +70,8 @@ export async function researcher(
-          )
+    <Section title="Answer">
+      <BotMessage content={streamText.value} />
+    </Section>
+  )
 
-          const searchResult =
-            searchAPI === 'tavily'
-              ? await tavilySearch(query, max_results, search_depth)
-              : await exaSearch(query)
+  let isFirstToolResponse = true
+  const result = await experimental_streamText({
+    model: openai.chat(process.env.OPENAI_API_MODEL || 'gpt-4-turbo'),
+    maxTokens: 2500,
+    system: `As a professional search expert, you possess the ability to search for any information on the web. 
+    For each user query, utilize the search results to their fullest potential to provide additional information and assistance in your response.
+    If there are any images relevant to your answer, be sure to include them as well.
+    Aim to directly address the user's question, augmenting your response with insights gleaned from the search results.
+    Whenever quoting or referencing information from a specific URL, always cite the source URL explicitly.
+    Please match the language of the response to the user's language.`,
+    messages,
+    tools: {
+      search: {
+        description: 'Search the web for information',
+        parameters: searchSchema,
+        execute: async ({
+          query,
+          max_results,
+          search_depth
+        }: {
+          query: string
+          max_results: number
+          search_depth: 'basic' | 'advanced'
+        }) => {
+          // If this is the first tool response, remove spinner
+          if (isFirstToolResponse) {
+            isFirstToolResponse = false
+            uiStream.update(null)
+          }
+          // Append the search section
+          const streamResults = createStreamableValue<string>()
+          uiStream.append(<SearchSection result={streamResults.value} />)
 
-          uiStream.update(
-	@@ -137,25 +136,24 @@ export async function researcher(
-  return { result, fullResponse }
+          // Tavily API requires a minimum of 5 characters in the query
+          const filledQuery =
+            query.length < 5 ? query + ' '.repeat(5 - query.length) : query
+          let searchResult
+          try {
+            searchResult =
+              searchAPI === 'tavily'
+                ? await tavilySearch(filledQuery, max_results, search_depth)
+                : await exaSearch(query)
+          } catch (error) {
+            console.error('Search API error:', error)
+            hasError = true
+          }
+
+          if (hasError) {
+            fullResponse += `\nAn error occurred while searching for "${query}.`
+            uiStream.update(
+              <Card className="p-4 mt-2 text-sm">
+                {`An error occurred while searching for "${query}".`}
+              </Card>
+            )
+            return searchResult
+          }
+
+          streamResults.done(JSON.stringify(searchResult))
+
+          return searchResult
+        }
+      }
+    }
+  })
+
+  const toolCalls: ToolCallPart[] = []
+  const toolResponses: ToolResultPart[] = []
+  for await (const delta of result.fullStream) {
+    switch (delta.type) {
+      case 'text-delta':
+        if (delta.textDelta) {
+          // If the first text delata is available, add a ui section
+          if (fullResponse.length === 0 && delta.textDelta.length > 0) {
+            // Update the UI
+            uiStream.update(answerSection)
+          }
+
+          fullResponse += delta.textDelta
+          streamText.update(fullResponse)
+        }
+        break
+      case 'tool-call':
+        toolCalls.push(delta)
+        break
+      case 'tool-result':
+        // Append the answer section if the specific model is not used
+        if (!useSpecificModel && toolResponses.length === 0) {
+          uiStream.append(answerSection)
+        }
+        toolResponses.push(delta)
+        break
+      case 'error':
+        hasError = true
+        fullResponse += `\nError occurred while executing the tool`
+        break
+    }
+  }
+  messages.push({
+    role: 'assistant',
+    content: [{ type: 'text', text: fullResponse }, ...toolCalls]
+  })
+
+  if (toolResponses.length > 0) {
+    // Add tool responses to the messages
+    messages.push({ role: 'tool', content: toolResponses })
+  }
+
+  return { result, fullResponse, hasError, toolResponses }
 }
 
 async function tavilySearch(
@@ -61,9 +170,11 @@ async function tavilySearch(
   if (!response.ok) {
     throw new Error(`Error: ${response.status}`)
   }
+
   const data = await response.json()
   return data
 }
+
 async function exaSearch(query: string, maxResults: number = 10): Promise<any> {
   const apiKey = process.env.EXA_API_KEY
   const exa = new Exa(apiKey)
